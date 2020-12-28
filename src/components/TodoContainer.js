fdb9e37131979191afe6198ca48112650596f05e
Samarth Roy <samarthroy2106@gmail.com>
2020-12-28 21:16:32 +0530

    Using axios to make get and post requests to dummy backend

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -3,27 +3,12 @@ import TodosList from "./TodosList";
 import Header from "./Header"
 import InputTodo from "./InputTodo"
 import {v4 as uuidv4} from "uuid"
+import axios from "axios"
 
 class TodoContainer extends React.Component {
 
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: "Setup development environment",
-        completed: true
-      },
-      {
-        id: uuidv4(),
-        title: "Develop website and add content",
-        completed: false
-      },
-      {
-        id: uuidv4(),
-        title: "Deploy to live server",
-        completed: false
-      }
-    ]
+    todos: []
   };
 
   handleChange = id => {
@@ -48,15 +33,21 @@ class TodoContainer extends React.Component {
   };
 
   addTodoItem = title => {
-    const newTodo = {
-      id: uuidv4(),
+    axios.post("https://jsonplaceholder.typicode.com/todos", {
       title: title,
-      completed: false
-    };
-    this.setState({
-      todos: [...this.state.todos, newTodo]
-    });
+      completed: false,
+    })
+    .then(response =>
+      this.setState({
+        todos: [...this.state.todos, response.data],
+      })
+    )
   };
+
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/todos?_limit=10")
+      .then(response => this.setState({todos: response.data}));
+  }
 
   render() {
     return (
